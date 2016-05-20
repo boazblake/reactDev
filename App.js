97abe70f727ec4223687874e2ade72cf737ab886
boazblake <boazblake@gmail.com>
2016-05-20 09:39:10 -0500

    class vs stateless functions

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-class App extends React.Component {
-  render(){
-  	return <div>HELLOW</div>
-  }
-}
+
+// Class Function
+// class App extends React.Component {
+//   render(){
+//   	return <h1>HELLOW WORLD</h1>
+//   }
+// }
+
+//StateLess Function
+	const App = () => <h1>HelloW orld</h1>
+
 
 export default App
