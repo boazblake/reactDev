c46f7d2d416eaa8e875c488b044edfeffc9ab714
boazblake <boazblake@gmail.com>
2016-05-20 10:38:00 -0500

    parent-child realtionships

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {txt: ''};
+		this.update = this.update.bind(this)
+	}
+
+	update(evt) {
+		this.setState({txt: evt.target.value})
+	}
   render(){
-  	return <div>HELLOW</div>
+  	return (
+  		<div>
+				<Widget txt={this.state.txt} update={this.update}/>
+				<Widget txt={this.state.txt} update={this.update}/>
+				<Widget txt={this.state.txt} update={this.update}/>
+  		</div>
+  	)
   }
+}
+
+
+const Widget = (props) => {
+	return (
+		<div>
+			<input type='text'
+				onChange={props.update} />
+				<h1>{props.txt}</h1>
+		</div>
+	)	
 }
 
 export default App
