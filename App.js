24b895b5884c3606e58ef8558d8aeda50588360f
boazblake <boazblake@gmail.com>
2016-05-20 11:27:03 -0500

    mounting and unmounting with state and props

--- App.js
+++ App.js
@@ -1,16 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
-  render(){
-  	return <Button>I <Heart/> REACT</Button>
+  constructor(){
+  	super();
+  	this.state = { val: 0 };
+  	this.update = this.update.bind(this);
+  }
+  update(){
+  	this.setState({ val: this.state.val +1 })
+  }
+
+	componentWillMount(){
+		console.log('mounting!')
+	}
+	componentDidMount(){
+		console.log('mounted!')
+	}
+  render() {
+  	console.log('rendering!')
+  	return <button onClick={this.update}>{this.state.val}</button>
+  }
+  componentWillUnmount(){
+  	console.log('bye-bye!')
   }
 }
 
-class Button extends React.Component {
+class Wrapper extends React.Component {
+	constructor(){
+		super();
+	}
+
+	mount(){
+		ReactDOM.render(<App/>, document.querySelector('#a'))
+	}
+
+	unmount(){
+		ReactDOM.unmountComponentAtNode(document.querySelector('#a'))
+	}
   render(){
-  	return <button>{this.props.children}</button>
+   return(
+   	<div>
+   		<button onClick={this.mount.bind(this)} className='btn btn-success'>MOUNT</button>
+   		<button onClick={this.unmount.bind(this)} className='btn btn-danger'>UNMOUNT</button>
+   		<div id='a'></div>
+   	</div>
+   ) 
   }
 }
 
-	const Heart = () => <span className='glyphicon glyphicon-heart'></span>
 
-export default App
+export default Wrapper
