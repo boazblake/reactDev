dc6a365f9c68aeef09bc0970cddb649cb8becb3e
boazblake <boazblake@gmail.com>
2016-05-20 10:10:19 -0500

    updating state

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {txt: 'this is the state text'}
+	}
+
+	update(e){
+		this.setState({txt:e.target.value})
+	}
+
   render(){
-  	return <div>HELLOW</div>
+  	return (
+  		<div>
+  			<input type='text' onChange={this.update.bind(this)} />
+  			<h1>{this.state.txt}</h1>
+  		</div>
+  		)
   }
+
 }
 
 export default App
