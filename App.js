9dfd63ca2daf0f817867f830daf0d191cd3d43e1
boazblake <boazblake@gmail.com>
2016-05-20 12:21:10 -0500

    mixins and life cycle components

--- App.js
+++ App.js
@@ -1,43 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component {
-  constructor(){
-  	super();
-  	this.state = { val: 0 };
-  	this.update = this.update.bind(this);
-  	this.state = {increasing: false}
-  }
 
-  update(){
-  	ReactDOM.render(
-  		<App val={this.props.val + 1} />,
-  		document.querySelector('#app')
+let Mixin = InnerComponent => class extends React.Component {
+	  constructor(){
+	  	super();
+	  	this.state = { val: 0 };
+	  	this.update = this.update.bind(this);
+	  }
+
+	  update(){
+		  this.setState({val: this.state.val + 1})
+	  }
+
+		componentWillMount(){
+		console.log('will mount')		
+		}
+
+		render() {
+			return <InnerComponent
+				update={this.update}
+				{...this.state}
+				{...this.props} />
+		}
+
+		componentDidMount() {
+		  console.log('mounted!')
+		}
+}
+
+
+const Button = (props) => <button 
+														onClick={props.update}>
+														{props.txt}: {props.val}
+													</button>
+
+const Label = (props) => <label 
+														onMouseMove={props.update}>
+														{props.txt}: {props.val}
+													</label>
+
+let ButtonMixed = Mixin(Button)
+let LabelMixed = Mixin(Label)
+
+class App extends React.Component {
+  render() {
+  	return (
+  		<div>
+  				<ButtonMixed txt='button'/>
+  				<br />
+  				<LabelMixed txt='label'/>
+  		</div>
   	);
   }
 
-  componentWillReceiveProps(nextProps){
-  	this.setState({increasing:nextProps.val > this.props.val})
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0 ;
-  }
-
-  render() {
-  	console.log(this.state.increasing)
-  	return (
-  		<button onClick={this.update}>
-  			{this.props.val}
-  		</button>)
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('prevProps', prevProps)
-    console.log('prevState', prevState)
-  }
 }
-
-App.defaultProps = { val: 0 }
-
 
 export default App
