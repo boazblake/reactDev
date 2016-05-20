7327795a129f1ffcd6b8cfe175e89218db3a4444
boazblake <boazblake@gmail.com>
2016-05-20 09:56:09 -0500

    react prop basics

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render(){
-  	return <div>HELLOW</div>
-  }
+  	let txt = this.props.txt
+  	return <div>{txt}</div>
+    }
 }
 
-export default App
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'sending a props will overirde this default text...loading...'
+}
+
+
+ReactDOM.render(<App cat={5}/>, document.querySelector('#app'))
